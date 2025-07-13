906df3d4db26ffa18044981160afe8a9336a331a
riseandshine0 <riseandshine@posteo.de>
2025-07-13 22:26:37 +0200

    L1: Print a Report

---
+++ main.py
@@ -0,0 +1,29 @@
+from stats import count_words, count_chars, sorted_dict
+
+def get_book_text(filepath):
+    contents = ''
+    with open(filepath) as f:
+        contents = f.read()
+    return contents
+
+def main():
+    print("============ BOOKBOT ============")
+    path = "books/frankenstein.txt"
+    print(f"Analyzing book found at {path}...")
+
+    num_words = count_words(get_book_text(path))
+
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+
+    print("--------- Character Count -------")
+    counted_chars = sorted_dict(count_chars(get_book_text(path)))
+    for elem in counted_chars:
+        if elem["char"].isalpha():
+            print(elem["char"]+":", elem["num"])
+
+    print("============= END ===============")
+    return 0
+
+main()
+
---
+++ stats.py
@@ -0,0 +1,27 @@
+def count_words(text):
+    word_counter = 0
+    for word in text.split():
+        word_counter += 1
+    return word_counter
+
+def count_chars(text):
+    counted_chars = {}
+    for word in text.split():
+        for char in word:
+            if char.lower() not in counted_chars:
+                counted_chars[char.lower()] = 1
+            else:
+                counted_chars[char.lower()] += 1 
+    return counted_chars # returns {'char': int}
+
+def sort_on(items):
+    # A function that takes a dictionary and returns the value of the "num" key
+    # This is how the `.sort()` method knows how to sort the list of dictionaries
+    return items["num"]
+
+def sorted_dict(dict_in):
+    new_list = []
+    for char_c, count in dict_in.items():
+        new_list.append({"char": char_c, "num": count})
+    new_list.sort(reverse=True, key=sort_on)    
+    return new_list
